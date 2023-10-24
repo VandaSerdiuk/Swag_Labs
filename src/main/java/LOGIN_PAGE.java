0941b22465608061231805307649f01cc8f7dd37
adik <adikdima29@gmail.com>
2023-10-24 09:27:25 -0400

    Привет- это для проверки

---
+++ src/main/java/LOGIN_PAGE.java
@@ -0,0 +1,52 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LOGIN_PAGE {
+    //                                                  Конструктор
+//                                                  privete WebDriver driver
+    private WebDriver driver;
+    public LOGIN_PAGE (WebDriver driver) {
+        this.driver = driver;
+
+    }
+    //                                  Нахожу все что нужно найти на сайте
+
+    private By Username = By.xpath("//input[@type='text']");
+    private By Password = By.xpath("//input[@id='password']");
+    private By LoginButton = By.xpath("//input[@id='login-button']");
+    private By SwagLabs = By.xpath("//div[text()='Swag Labs']");
+    private By SauceLabsBackpack = By.xpath("//div[text()='Sauce Labs Backpack']");
+
+
+
+    //                                      Методы с помощью которых  xPath начинает работать
+    public LOGIN_PAGE TypeUsername (String username) {
+        driver.findElement(Username).sendKeys(username);
+        return this;
+    }
+    public LOGIN_PAGE TypePassword (String password) {
+        driver.findElement(Password).sendKeys(password);
+        return this;
+    }
+    public LOGIN_PAGE TypeSauceLabsBackpack (String password) {
+        driver.findElement(SauceLabsBackpack).sendKeys(password);
+        return this;
+    }
+
+
+
+    //                                          Метод для входа вписанный в один код
+    public LOGIN_PAGE ToSignIn (String USER_NAME, String PASSWORD){
+        this.TypeUsername(USER_NAME);
+        this.TypePassword(PASSWORD);
+        driver.findElement(LoginButton).click();
+        return new LOGIN_PAGE(driver);
+    }
+
+
+// проверка текста если перешел на страницу
+    public String getSwagLabs () {
+        return driver.findElement(SwagLabs).getText();
+    }
+}
+
---
+++ src/test/java/TEST_LOGIN_PAGE.java
@@ -0,0 +1,43 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class TEST_LOGIN_PAGE {
+
+
+    private WebDriver driver;
+    private LOGIN_PAGE LOGINPAGE;
+
+    @Before
+    public void setUp() {
+        System.setProperty("WebDriver.chrome.driver",
+                "/Users/adik/Downloads/Selenium instal.zip/Chromedriver-mac-arm64/116.0.5845.96/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+        LOGINPAGE = new LOGIN_PAGE(driver);
+    }
+
+    @Test
+    public void SignInPass() {
+      LOGIN_PAGE loginpage = LOGINPAGE.ToSignIn("standard_user","secret_sauce");
+      String error = loginpage.getSwagLabs();
+      Assert.assertEquals("Swag Labs",error);
+    }
+    @Test
+    public void SwagLabs() {
+        LOGIN_PAGE loginpage = LOGINPAGE.ToSignIn("standard_user","secret_sauce");
+
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
